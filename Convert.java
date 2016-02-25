a28ed3ffe1b932dcaea36d576bfe109947c00fa4
tebastasch <697278@211-167-S-31.athena.root>
2016-02-25 15:37:54 -0800

    Test Upload
    Testing making commits and pull requests

--- Convert.java
+++ Convert.java
@@ -8,4 +8,22 @@ public class Convert {
 	public static double CelsiusToFahrenheit(double cels){
 		return cels * (9/5.0) + 32;
 	}
+	
+	public static int decimalToBase(int num, int base)
+	{
+		if (base == 10)
+		{
+			return num;
+		}
+		String tmp = "";
+		
+		while(num != 0)
+		{
+			int rem = num % base;
+			tmp += rem;
+			num /= base;
+		}
+		// TODO: Reverse the string
+		return (tmp.equals("") ? 0 : Integer.parseInt(tmp));
+	}
 }
